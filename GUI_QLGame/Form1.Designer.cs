08ecec96e7989f9be195272fb8462358fcb7226b
NGQuan1207 <minhquan12072002@gmail.com>
2024-07-12 18:05:44 +0700

    QUAN

--- GUI_QLGame/Form1.Designer.cs
+++ GUI_QLGame/Form1.Designer.cs
@@ -28,36 +28,21 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.checkBox2 = new System.Windows.Forms.CheckBox();
             this.SuspendLayout();
-            // 
-            // checkBox2
-            // 
-            this.checkBox2.AutoSize = true;
-            this.checkBox2.Location = new System.Drawing.Point(171, 229);
-            this.checkBox2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
-            this.checkBox2.Name = "checkBox2";
-            this.checkBox2.Size = new System.Drawing.Size(95, 20);
-            this.checkBox2.TabIndex = 1;
-            this.checkBox2.Text = "checkBox2";
-            this.checkBox2.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1077, 564);
-            this.Controls.Add(this.checkBox2);
             this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
         #endregion
-        private System.Windows.Forms.CheckBox checkBox2;
     }
 }
 
